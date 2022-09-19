f92f3776dba2afa64cb51ab4a0922a04153fc83c
Alexei Garban <garban.valdeon@gmail.com>
2022-09-19 16:05:01 +0200

    new route

--- routes/postEventReport.js
+++ routes/postEventReport.js
@@ -8,6 +8,7 @@ router.get("/",controller.getAllPostEventReports)
 router.get("/nys_events_output",controller.getNysEventsOutput)
 router.get("/:id",controller.getPostEventReport)
 router.get("/event/:id",controller.getPostEventReportById)
+router.put("/update",controller.updatePostEventReport)
 
 
 module.exports = router
