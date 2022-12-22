f33f3333ed2ddeb6804e46bb3306924de83aaeaa
Alexei Garban <garban.valdeon@gmail.com>
2022-12-22 17:16:50 +0100

    new route of fbos

--- controllers/fbosControllers.js
+++ controllers/fbosControllers.js
@@ -9,5 +9,145 @@ module.exports= {
       } catch (e) {
         res.send("an error ocurred");
       }
-}
+},
+createFbo: async (req,res)=>{
+  let {nameFBO,
+    addressFBO,
+    boroughFBO,
+    zipcodeFBO,	
+    nameReligiousLeader,	
+    emailReligionsLeader,	
+    phoneReligionsLeader,	
+    nameKeyContact ,	
+    phoneKeyContact ,	
+    emailKeyContact ,	
+    nameAlternateContact ,	
+    phoneAlternateContact ,	
+    emailAlternateContact ,	
+    fboDropboxFolder ,	
+    fboNotes ,	
+    linkedAccounts ,	
+    fboActive } = req.body
+
+    const query = {
+      text: `INSERT INTO fbos (nameFBO,
+        addressFBO,
+        boroughFBO,
+        zipcodeFBO,	
+        nameReligiousLeader,	
+        emailReligionsLeader,	
+        phoneReligionsLeader,	
+        nameKeyContact ,	
+        phoneKeyContact ,	
+        emailKeyContact ,	
+        nameAlternateContact ,	
+        phoneAlternateContact ,	
+        emailAlternateContact ,	
+        fboDropboxFolder ,	
+        fboNotes ,	
+        linkedAccounts ,	
+        fboActive) VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17) RETURNING *`,
+      values: [nameFBO,
+        addressFBO,
+        boroughFBO,
+        zipcodeFBO,	
+        nameReligiousLeader,	
+        emailReligionsLeader,	
+        phoneReligionsLeader,	
+        nameKeyContact ,	
+        phoneKeyContact ,	
+        emailKeyContact ,	
+        nameAlternateContact ,	
+        phoneAlternateContact ,	
+        emailAlternateContact ,	
+        fboDropboxFolder ,	
+        fboNotes ,	
+        linkedAccounts ,	
+        fboActive],
+    }
+    // promise
+    db
+      .query(query)
+      .then(data => res.status(200).json(data.rows[0]))
+      .then(response=> console.log("fbos created"))
+      .catch(e => console.error(e.stack))
+},
+updateFbo: async (req, res) => {
+  let {
+    numberfbo,
+    nameFBO,
+    addressFBO,
+    boroughFBO,
+    zipcodeFBO,	
+    nameReligiousLeader,	
+    emailReligionsLeader,	
+    phoneReligionsLeader,	
+    nameKeyContact ,	
+    phoneKeyContact ,	
+    emailKeyContact ,	
+    nameAlternateContact ,	
+    phoneAlternateContact ,	
+    emailAlternateContact ,	
+    fboDropboxFolder ,	
+    fboNotes ,	
+    linkedAccounts ,	
+    fboActive} = req.body;
+
+
+  try {
+    const query = await {
+      name: "update-fbo",
+      text: `update fbos set 
+      numberfbo=$1,
+      nameFBO=$2,
+      addressFBO=$3,
+      boroughFBO=$4,
+      zipcodeFBO=$5,	
+      nameReligiousLeader=$6,	
+      emailReligionsLeader=$7,	
+      phoneReligionsLeader=$8,	
+      nameKeyContact=$9 ,	
+      phoneKeyContact=$10 ,	
+      emailKeyContact=$11 ,	
+      nameAlternateContact=$12 ,	
+      phoneAlternateContact=$13 ,	
+      emailAlternateContact=$14 ,	
+      fboDropboxFolder=$15 ,	
+      fboNotes=$16 ,	
+      linkedAccounts=$17 ,	
+      fboActive=$18 where numberfbo=$1`,
+      values: [numberfbo,
+        nameFBO,
+        addressFBO,
+        boroughFBO,
+        zipcodeFBO,	
+        nameReligiousLeader,	
+        emailReligionsLeader,	
+        phoneReligionsLeader,	
+        nameKeyContact ,	
+        phoneKeyContact ,	
+        emailKeyContact ,	
+        nameAlternateContact ,	
+        phoneAlternateContact ,	
+        emailAlternateContact ,	
+        fboDropboxFolder ,	
+        fboNotes ,	
+        linkedAccounts ,	
+        fboActive],
+    };
+    db
+      .query(query)
+      .then((response) =>
+        res.json({
+          data: response.rowCount,
+          status: 200,
+        })
+        .then((response) => console.log("fbo updated"))
+      )
+      //.catch((e) => res.send(e.stack));
+  } catch (error) {
+    res.json("an error ocurred");
+    console.log("error message:", error);
+  }
+},
 }
--- routes/fbos.js
+++ routes/fbos.js
@@ -4,6 +4,8 @@ const router = express.Router()
 const controller = require('../controllers/fbosControllers')
 
 router.get("/",controller.getFbos)
+router.post("/",controller.createFbo)
+router.put("/",controller.updateFbo)
 
 module.exports = router
 
