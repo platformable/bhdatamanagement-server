78ec18f8e765a958931c6dac0798159e7a842251
leoncan122 <leon.can122@gmail.com>
2022-12-22 23:57:36 +0100

    Update create fbo controller

--- controllers/fbosControllers.js
+++ controllers/fbosControllers.js
@@ -1,40 +1,45 @@
 const db = require("../dbConnect");
 
-module.exports= {
-    getFbos: async (req,res)=>{
+module.exports = {
+  getFbos: async (req, res) => {
     try {
-        const allData = await db.query("select * from fbos");
-        const response = allData.rows;
-        res.send(response);
-      } catch (e) {
-        res.send("an error ocurred");
-      }
-},
-createFbo: async (req,res)=>{
-  let {nameFBO,
-    addressFBO,
-    boroughFBO,
-    zipcodeFBO,	
-    nameReligiousLeader,	
-    emailReligionsLeader,	
-    phoneReligionsLeader,	
-    nameKeyContact ,	
-    phoneKeyContact ,	
-    emailKeyContact ,	
-    nameAlternateContact ,	
-    phoneAlternateContact ,	
-    emailAlternateContact ,	
-    fboDropboxFolder ,	
-    fboNotes ,	
-    linkedAccounts ,	
-    fboActive } = req.body
+      const allData = await db.query("select * from fbos");
+      const response = allData.rows;
+      res.send(response);
+    } catch (e) {
+      res.send("an error ocurred");
+    }
+  },
+  createFbo: async (req, res) => {
+    let {
+      nameFBO,
+      addressFBO,
+      address2FBO,
+      boroughFBO,
+      zipcodeFBO,
+      nameReligiousLeader,
+      positionReligiousLeader,
+      emailReligionsLeader,
+      phoneReligionsLeader,
+      nameKeyContact,
+      phoneKeyContact,
+      emailKeyContact,
+      nameAlternateContact,
+      phoneAlternateContact,
+      emailAlternateContact,
+      fboDropboxFolder,
+      fboNotes,
+      fboActive,
+    } = req.body;
 
     const query = {
       text: `INSERT INTO fbos (nameFBO,
         addressFBO,
+        address2FBO,
         boroughFBO,
         zipcodeFBO,	
         nameReligiousLeader,	
+        positionReligiousLeader,	
         emailReligionsLeader,	
         phoneReligionsLeader,	
         nameKeyContact ,	
@@ -45,59 +50,60 @@ createFbo: async (req,res)=>{
         emailAlternateContact ,	
         fboDropboxFolder ,	
         fboNotes ,	
-        linkedAccounts ,	
-        fboActive) VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17) RETURNING *`,
-      values: [nameFBO,
+        fboActive) VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17, $18) RETURNING *`,
+      values: [
+        nameFBO,
         addressFBO,
+        address2FBO,
         boroughFBO,
-        zipcodeFBO,	
-        nameReligiousLeader,	
-        emailReligionsLeader,	
-        phoneReligionsLeader,	
-        nameKeyContact ,	
-        phoneKeyContact ,	
-        emailKeyContact ,	
-        nameAlternateContact ,	
-        phoneAlternateContact ,	
-        emailAlternateContact ,	
-        fboDropboxFolder ,	
-        fboNotes ,	
-        linkedAccounts ,	
-        fboActive],
-    }
+        zipcodeFBO,
+        nameReligiousLeader,
+        emailReligionsLeader,
+        positionReligiousLeader,
+        phoneReligionsLeader,
+        nameKeyContact,
+        phoneKeyContact,
+        emailKeyContact,
+        nameAlternateContact,
+        phoneAlternateContact,
+        emailAlternateContact,
+        fboDropboxFolder,
+        fboNotes,
+        fboActive,
+      ],
+    };
     // promise
-    db
-      .query(query)
-      .then(data => res.status(200).json(data.rows[0]))
-      .then(response=> console.log("fbos created"))
-      .catch(e => console.error(e.stack))
-},
-updateFbo: async (req, res) => {
-  let {
-    numberfbo,
-    nameFBO,
-    addressFBO,
-    boroughFBO,
-    zipcodeFBO,	
-    nameReligiousLeader,	
-    emailReligionsLeader,	
-    phoneReligionsLeader,	
-    nameKeyContact ,	
-    phoneKeyContact ,	
-    emailKeyContact ,	
-    nameAlternateContact ,	
-    phoneAlternateContact ,	
-    emailAlternateContact ,	
-    fboDropboxFolder ,	
-    fboNotes ,	
-    linkedAccounts ,	
-    fboActive} = req.body;
+    db.query(query)
+      .then((data) => res.status(200).json(data.rows[0]))
+      .then((response) => console.log("fbos created"))
+      .catch((e) => console.error(e.stack));
+  },
+  updateFbo: async (req, res) => {
+    let {
+      numberfbo,
+      nameFBO,
+      addressFBO,
+      boroughFBO,
+      zipcodeFBO,
+      nameReligiousLeader,
+      emailReligionsLeader,
+      phoneReligionsLeader,
+      nameKeyContact,
+      phoneKeyContact,
+      emailKeyContact,
+      nameAlternateContact,
+      phoneAlternateContact,
+      emailAlternateContact,
+      fboDropboxFolder,
+      fboNotes,
+      linkedAccounts,
+      fboActive,
+    } = req.body;
 
-
-  try {
-    const query = await {
-      name: "update-fbo",
-      text: `update fbos set 
+    try {
+      const query = await {
+        name: "update-fbo",
+        text: `update fbos set 
       numberfbo=$1,
       nameFBO=$2,
       addressFBO=$3,
@@ -116,38 +122,39 @@ updateFbo: async (req, res) => {
       fboNotes=$16 ,	
       linkedAccounts=$17 ,	
       fboActive=$18 where numberfbo=$1`,
-      values: [numberfbo,
-        nameFBO,
-        addressFBO,
-        boroughFBO,
-        zipcodeFBO,	
-        nameReligiousLeader,	
-        emailReligionsLeader,	
-        phoneReligionsLeader,	
-        nameKeyContact ,	
-        phoneKeyContact ,	
-        emailKeyContact ,	
-        nameAlternateContact ,	
-        phoneAlternateContact ,	
-        emailAlternateContact ,	
-        fboDropboxFolder ,	
-        fboNotes ,	
-        linkedAccounts ,	
-        fboActive],
-    };
-    db
-      .query(query)
-      .then((response) =>
-        res.json({
-          data: response.rowCount,
-          status: 200,
-        })
-        .then((response) => console.log("fbo updated"))
-      )
+        values: [
+          numberfbo,
+          nameFBO,
+          addressFBO,
+          boroughFBO,
+          zipcodeFBO,
+          nameReligiousLeader,
+          emailReligionsLeader,
+          phoneReligionsLeader,
+          nameKeyContact,
+          phoneKeyContact,
+          emailKeyContact,
+          nameAlternateContact,
+          phoneAlternateContact,
+          emailAlternateContact,
+          fboDropboxFolder,
+          fboNotes,
+          linkedAccounts,
+          fboActive,
+        ],
+      };
+      db.query(query).then((response) =>
+        res
+          .json({
+            data: response.rowCount,
+            status: 200,
+          })
+          .then((response) => console.log("fbo updated"))
+      );
       //.catch((e) => res.send(e.stack));
-  } catch (error) {
-    res.json("an error ocurred");
-    console.log("error message:", error);
-  }
-},
-}
+    } catch (error) {
+      res.json("an error ocurred");
+      console.log("error message:", error);
+    }
+  },
+};
