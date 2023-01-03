8822edad0f2a836ac94c82e8ebcffa81d160daa4
Alexei Garban <garban.valdeon@gmail.com>
2023-01-03 14:59:58 +0100

    change endpoint name

--- routes/participantEventOutputs.js
+++ routes/participantEventOutputs.js
@@ -4,7 +4,7 @@ const controller= require("../controllers/participantEventOutputsControllers")
 
 
 router.post("/create",controller.createParticipantEventOutputs)
-router.post("/oef/create",controller.createOefParticipantEventOutputs)
+router.post("/oef/participant-event-survey/create",controller.createOefParticipantEventOutputs)
 router.get("/",controller.getParticipantEventOutputs)
 router.get("/:id",controller.getParticipantEventOutputsById)
 
