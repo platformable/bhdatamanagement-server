f10d224a1f3d532d5c9a4c5eef16961d4ad2eeb0
Alexei Garban <garban.valdeon@gmail.com>
2023-02-02 17:40:09 +0100

    added oef cbt quarterly evaluation endpoint

--- routes/participantEventOutputs.js
+++ routes/participantEventOutputs.js
@@ -6,6 +6,7 @@ const controller= require("../controllers/participantEventOutputsControllers")
 router.post("/create",controller.createParticipantEventOutputs)
 router.post("/oef/participant-event-survey/create",controller.createOefParticipantEventOutputs)
 router.post("/oef/cbt/participant-event-survey/create",controller.createOefCbtParticipantEventOutputs)
+router.post("/oef-cbt-quarterly-evaluation-survey/create",controller.createOefCbtQuarterlyEvaluationSurvey)
 router.get("/",controller.getParticipantEventOutputs)
 router.get("/:id",controller.getParticipantEventOutputsById)
 
