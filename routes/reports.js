4a93efef5eeeed6e95b9bc9ac984bd8ba21d9436
Alexei Garban <garban.valdeon@gmail.com>
2023-02-13 17:54:37 +0100

    oef hiv outreach csv data

--- routes/reports.js
+++ routes/reports.js
@@ -5,8 +5,24 @@ const controller= require("../controllers/reportsControllers")
 
 router.get("/oef/fbo/participant_survey",controller.getOefFboParticipantEventsOutput)
 router.get("/oef/cbt/participant_survey",controller.getOefCbtParticipantEventsOutput)
+router.get("/oef/hiv/fbo_outreach",controller.getOefHivOutReach)
 
 
 
 module.exports = router
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
