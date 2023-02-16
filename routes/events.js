04936e84831e93eaf8a82ec5b08dba34c2e37bfd
Alexei Garban <garban.valdeon@gmail.com>
2023-02-16 11:11:44 +0100

    new routes to print data

--- routes/events.js
+++ routes/events.js
@@ -2,10 +2,13 @@
 const express = require('express')
 const router = express.Router()
 const controller = require('../controllers/eventsControllers')
+const printController = require('../controllers/eventsPrintControllers')
 
 router.get("/",controller.getEvents)
 router.get("/:id",controller.getEventById)
 router.get("/oef/cab",controller.getOefCabEvents)
+router.get("/oef/hiv/data_to_print/:id",printController.getHivOutreachDataToPrint)
+router.get("/oef/cab/data_to_print/:id",printController.getCabDataToPrint)
 router.post("/",controller.createEvent)
 router.post("/oef/create",controller.createOefEvent)
 router.post("/oef/cbt/create",controller.createOefCbtEvent)
