25d65a9d646a801d2ee9b5da44a7135552c35fc1
Alexei Garban <garban.valdeon@gmail.com>
2023-03-29 15:40:57 +0200

    added session3

--- routes/participantEventOutputs.js
+++ routes/participantEventOutputs.js
@@ -9,6 +9,8 @@ router.post("/oef/cbt/participant-event-survey/create",controller.createOefCbtPa
 router.post("/oef-cbt-quarterly-evaluation-survey/create",controller.createOefCbtQuarterlyEvaluationSurvey)
 router.post("/oef-pre-workout-survey/create",controller.createOefPreWorkshopSurvey)
 router.post("/oef-yip-participant-session1-survey/create",controller.createOefYipParticipantSession1)
+router.post("/oef-yip-participant-session2-survey/create",controller.createOefYipParticipantSession2)
+router.post("/oef-yip-participant-session3-survey/create",controller.createOefYipParticipantSession3)
 router.get("/",controller.getParticipantEventOutputs)
 router.get("/:id",controller.getParticipantEventOutputsById)
 router.get("/oef/cbt",controller.getParticipantOEFCBTEventOutputs)
