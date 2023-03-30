9ebb06376534048d1ba1e12f1097e8f859a23906
Alexei Garban <garban.valdeon@gmail.com>
2023-03-30 16:09:17 +0200

    6 months follow up added

--- routes/participantEventOutputs.js
+++ routes/participantEventOutputs.js
@@ -12,6 +12,7 @@ router.post("/oef-yip-participant-session1-survey/create",controller.createOefYi
 router.post("/oef-yip-participant-session2-survey/create",controller.createOefYipParticipantSession2)
 router.post("/oef-yip-participant-session3-survey/create",controller.createOefYipParticipantSession3)
 router.post("/oef-yip-participant-session4-survey/create",controller.createOefYipParticipantSession4)
+router.post("/oef-yip-6month-follow-up/create",controller.createOefYip6MonthsFollowUp)
 router.get("/",controller.getParticipantEventOutputs)
 router.get("/:id",controller.getParticipantEventOutputsById)
 router.get("/oef/cbt",controller.getParticipantOEFCBTEventOutputs)
